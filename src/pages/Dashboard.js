d58eb13f401cada5bf07c5e9966cd85f8de4e2e9
kiran.kafle <kiran@aegis.com.np>
2022-06-07 09:24:43 +0545

    Post Req Demo Done

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,9 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function Dashboard(props) {
+
+    const [studentList,setStudentList]=useState([]);
+
+    useEffect(()=>{
+        fetch('http://localhost:5001/student/getallstudent')
+        .then(response => response.json())
+        .then(data => {
+            setStudentList(data);
+        });
+    },[])
+
+    const sendData=()=>{
+        var data={
+            id:8,
+            name:"Ramesh"
+        }
+
+        const rawResponse = fetch('http://localhost:5001/student/create', {
+            method: 'POST',
+            headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        }).then(res=>res.json())
+        .then(data=>{
+            console.log(data)
+        });
+    }
+
     return (
         <>
-            Dashboard
+            {studentList.map(m=>(<li>{m.name}</li>))}
+            <br/>
+            <button onClick={()=>sendData()}>Test Create API</button>
         </>
     );
 }
